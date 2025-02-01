13b603ec09bf84c7278e4ec6a8b17235d2ecb174
Ewerton <Ewerton@DESKTOP-MN9GPG8>
2025-02-01 14:06:29 -0300

    ajustes para o deploy

--- deploy-container.bat
+++ deploy-container.bat
@@ -8,7 +8,7 @@ REM Solicitar Nome do Container
 set /p CONTAINER_NAME="Enter the container name: "
 
 REM Solicitar Nome da Imagem
-set /p IMAGE_NAME="Enter the image name: "
+REM set /p IMAGE_NAME="Enter the image name: "
 
 REM Solicitar Porta da Aplicação
 set /p CONTAINER_PORT="Enter the port for the application: "
@@ -17,14 +17,11 @@ REM Exibindo as configurações
 echo ========================================
 echo Server IP: %SERVER_IP%
 echo Container Name: %CONTAINER_NAME%
-echo Image Name: %IMAGE_NAME%
+REM echo Image Name: %IMAGE_NAME%
 echo Application Port: %CONTAINER_PORT%
 echo ========================================
 
 REM Conectando ao servidor via SSH e executando os comandos
-ssh root@%SERVER_IP% ^
-"cd docker && ^
-docker load -i %CONTAINER_NAME%.tar && ^
-docker run -d -p %CONTAINER_PORT%:%CONTAINER_PORT% --name %CONTAINER_NAME% %IMAGE_NAME%"
+ssh root@%SERVER_IP% "cd docker && docker load -i %CONTAINER_NAME%.tar && docker run -d -p %CONTAINER_PORT%:3000 --name %CONTAINER_NAME% %CONTAINER_NAME%"
 
 pause
