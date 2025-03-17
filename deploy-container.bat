5e7e0ccd2318182c5cd256e780b73eb7957dc9b0
Ewerton <Ewerton@DESKTOP-MN9GPG8>
2025-03-17 12:51:54 -0300

    ajustes nos scripts de deploy e renovação para solicitar o diretório do projeto e definir o nome do container automaticamente

--- deploy-container.bat
+++ deploy-container.bat
@@ -4,20 +4,29 @@ setlocal enabledelayedexpansion
 REM Definir IP do Servidor
 set SERVER_IP=51.79.39.251
 
-REM Solicitar Nome do Container
-set /p CONTAINER_NAME="Enter the container name: "
+REM Solicitar o diretório do projeto
+set /p PROJECT_DIR="Enter the project directory: "
+if not exist "%PROJECT_DIR%" (
+    echo Directory does not exist. Exiting...
+    exit /b
+)
 
-REM Solicitar Nome da Imagem
-REM set /p IMAGE_NAME="Enter the image name: "
+REM Obter o nome da pasta como CONTAINER_NAME e IMAGE_NAME
+for %%i in ("%PROJECT_DIR%") do set CONTAINER_NAME=%%~ni
+
+REM Converter o nome do container para minúsculas
+for /f %%a in ('powershell -Command "$('%CONTAINER_NAME%'.ToLower())"') do set CONTAINER_NAME=%%a
 
 REM Solicitar Porta da Aplicação
 set /p CONTAINER_PORT="Enter the port for the application: "
 
+REM Solicitar Porta Interna da Aplicação
 set /p INTERNAL_PORT="Enter the internal port for the application: "
 
 REM Exibindo as configurações
 echo ========================================
 echo Server IP: %SERVER_IP%
+echo Project Directory: %PROJECT_DIR%
 echo Container Name: %CONTAINER_NAME%
 echo Application Port: %CONTAINER_PORT%
 echo Application Internal Port: %INTERNAL_PORT%
--- renew container.bat
+++ renew container.bat
@@ -4,11 +4,21 @@ setlocal enabledelayedexpansion
 REM Definir IP do Servidor
 set SERVER_IP=51.79.39.251
 
-REM Solicitar Nome do Container
-set /p CONTAINER_NAME="Enter the container name: "
+REM Solicitar o diretório do projeto
+set /p PROJECT_DIR="Enter the project directory: "
+if not exist "%PROJECT_DIR%" (
+    echo Directory does not exist. Exiting...
+    exit /b
+)
 
-REM Solicitar Nome da Imagem
-set /p IMAGE_NAME="Enter the image name: "
+REM Obter o nome da pasta como CONTAINER_NAME e IMAGE_NAME
+for %%i in ("%PROJECT_DIR%") do set CONTAINER_NAME=%%~ni
+
+REM Converter o nome do container para minúsculas
+for /f %%a in ('powershell -Command "$('%CONTAINER_NAME%'.ToLower())"') do set CONTAINER_NAME=%%a
+
+REM Definir IMAGE_NAME igual ao CONTAINER_NAME
+set IMAGE_NAME=%CONTAINER_NAME%
 
 REM Solicitar Porta da Aplicação
 set /p CONTAINER_PORT="Enter the port for the application: "
@@ -16,17 +26,18 @@ set /p CONTAINER_PORT="Enter the port for the application: "
 REM Exibindo as configurações
 echo ========================================
 echo Server IP: %SERVER_IP%
+echo Project Directory: %PROJECT_DIR%
 echo Container Name: %CONTAINER_NAME%
 echo Image Name: %IMAGE_NAME%
 echo Application Port: %CONTAINER_PORT%
 echo ========================================
 
 REM Conectando ao servidor via SSH e executando os comandos
-ssh root@%SERVER_IP% ^
-"docker ps && ^
-docker stop %CONTAINER_NAME% && docker rm %CONTAINER_NAME% -f && ^
-docker rmi %IMAGE_NAME% && ^
-cd ~/docker && docker load -i %IMAGE_NAME%.tar && ^
+ssh root@%SERVER_IP% ^ 
+"docker ps && ^ 
+docker stop %CONTAINER_NAME% && docker rm %CONTAINER_NAME% -f && ^ 
+docker rmi %IMAGE_NAME% && ^ 
+cd ~/docker && docker load -i %IMAGE_NAME%.tar && ^ 
 docker run -d -p %CONTAINER_PORT%:3000 --name %CONTAINER_NAME% %IMAGE_NAME%"
 
 pause
